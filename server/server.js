39e4525089dfab9b7033ce383cfb2d698884a53d
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-15 14:06:42 -0300

    Teste inicial do Servidor

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
-var express = require('express')
-var app = express()
+const express = require('express');
+const app = express();
  
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
-app.listen(3000)
+app.listen(3000, () =>{
+    console.log('Escutando na porta: ', 3000);
+});
+
+//Realize o teste com o comando: node server/server
+//No Postman execute GET: http://localhost:3000
+//Verifique em headers que o formato de retorno é Content-Type →text/html; charset=utf-8
+//Porém queremos que retorne formato JSON
