6184c1d8d6f66120448b0906712ca8a53f80e0bf
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-15 15:24:10 -0300

    Configurando o PUT

--- server/server.js
+++ server/server.js
@@ -3,24 +3,30 @@ const app = express();
 
 //Consultar usuário
 app.get('/usuario', function (req, res) {
-  res.json('get Usuário funcionando')
+  res.json('get Usuário funcionando');
 });
 //Criar usuário
 app.post('/usuario', function (req, res) {
-    res.json('post Usuário funcionando')
+    res.json('post Usuário funcionando');
   });
 //Atualizar usuário
-app.put('/usuario', function (req, res) {
-res.json('put Usuário funcionando')
+app.put('/usuario/:id', function (req, res) {
+    let id = req.params.id;
+    //Retorne o que mandei na url
+    res.json({
+        id
+    });
 });
 //Excluir usuário
 app.delete('/usuario', function (req, res) {
-res.json('delete Usuário funcionando')
+    res.json('delete Usuário funcionando');
 });
  
 app.listen(3000, () =>{
     console.log('Escutando na porta: ', 3000);
 });
 
-//No Postman, realize testes com cada um dos verbos HTTP em:
-//http://localhost:3000/usuario
+//No Postman, realize testes com alterando valores no local de id:
+//Exemplos:
+    //http://localhost:3000/usuario/1354
+    //http://localhost:3000/usuario/1208
