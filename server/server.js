99cd6e9df3b288e0b0e506fefcc26473bbe74594
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-15 14:15:28 -0300

    Transformando o formato de retorno em JSON

--- server/server.js
+++ server/server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.json('Hello World')
 })
  
 app.listen(3000, () =>{
     console.log('Escutando na porta: ', 3000);
 });
 
-//Realize o teste com o comando: node server/server
-//No Postman execute GET: http://localhost:3000
-//Verifique em headers que o formato de retorno é Content-Type →text/html; charset=utf-8
-//Porém queremos que retorne formato JSON
+//Restarte o servidor com nodemon para restart automático em atualizações: 
+    //Ctrl + C 
+    //nodemon server/server
+//No Postman execute novamente GET: http://localhost:3000
+//Verifique em headers que agora o formato de retorno é Content-Type →application/json; charset=utf-8
