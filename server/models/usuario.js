d95d6d7a51097ba7990dcf77e366e5d1be912275
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-16 00:20:57 -0300

    POST: Criação de Usuário e Validações

---
+++ server/models/usuario.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let usuarioSchema = new Schema({
+    nome:{
+        type: String,
+        required: [true, 'O nome é necessário']
+    },
+    email:{
+        type: String,
+        required:[true, 'O e-mail é necessário']
+    },
+    password:{
+        type: String,
+        required: [true, 'A senha é obrigatória']
+    },
+    img:{
+        type: String,
+        required: false
+    }, 
+    role:{
+        type:String,
+        default: 'USER_ROLE'
+    },
+    estado:{
+        type: Boolean,
+        default: true
+    },
+    google:{
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,4 +1,6 @@
 const express = require('express');
+//Objeto Usuário para trabalhar com os Schemas
+const Usuario = require('../models/usuario');
 const app = express();
 
 //Consultar usuário
@@ -11,18 +13,77 @@ app.post('/usuario', function (req, res) {
     //um payload que receba as petições
     let body = req.body;
 
-    //Validação de formulário
-    if(body.nome === undefined){
-        res.status(400).json({
-            ok:false,
-            mensagem: 'O nome é necessário'
-        });
-    }else{
+    let usuario = new Usuario({
+        nome: body.nome,
+        email: body.email,
+        password: body.password,
+        role: body.role
+    });
+
+    usuario.save((err, usuarioDB) =>{
+        if(err){
+            return res.status(400).json({
+                ok:false,
+                err
+            });
+        }
         res.json({
-            usuario:body
+            ok: true,
+            usuario: usuarioDB
         });
-    }
+    });
 });
+//Teste no Postman enviando somente os dados nome, idade e email
+//Retorno: 400 Bad Request com o JSON dos erros:
+// {
+//     "ok": false,
+//     "err": {
+//         "errors": {
+//             "password": {
+//                 "message": "A senha é obrigatória",
+//                 "name": "ValidatorError",
+//                 "properties": {
+//                     "message": "A senha é obrigatória",
+//                     "type": "required",
+//                     "path": "password"
+//                 },
+//                 "kind": "required",
+//                 "path": "password",
+//                 "$isValidatorError": true
+//             }
+//         },
+//         "_message": "Usuario validation failed",
+//         "message": "Usuario validation failed: password: A senha é obrigatória",
+//         "name": "ValidationError"
+//     }
+// }
+
+//Teste novamente colocando todos os dados obrigatórios
+//Retorno:
+// {
+//     "ok": true,
+//     "usuario": {
+//         "role": "USER_ROLE",
+//         "estado": true,
+//         "google": false,
+//         "_id": "5b74eb4ffc57e13b48b54136",
+//         "nome": "Rodrigo",
+//         "email": "conectarodrigo@gmail.com",
+//         "password": "123456",
+//         "__v": 0
+//     }
+// }
+//Starte o Mongo DB, Execute: sudo mongod
+//Abra Robo 3T e verifique que agora temos a base de dados "cafe" criada
+//E um objeto usuário registrado na tabela usuários
+//No Postman salve a requisição em save
+//Renomeie: POST: criar usuário
+//Descrição: Criamos um usuário de BD
+//Create Collection
+//Nome: Cafe-Udemy
+//Selecione Cafe-Udemy
+//Salvar to Cafe-Udemy
+
 //Atualizar usuário
 app.put('/usuario/:id', function (req, res) {
     let id = req.params.id;
