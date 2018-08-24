aca120ec5897a8517faa5b535d00f472eb05db6e
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-24 13:37:09 -0300

    POST: obtendo os dados do body

--- server/routes/categoria.js
+++ server/routes/categoria.js
@@ -7,7 +7,7 @@ let Categoria = require('../models/categoria');
 // Cria nova categoria
 // ===========================
 app.post('/categorias', verificaToken, (req, res)=>{
-
+    let body = req.body;
     //Retorna a nova categoria
     // Através da função verificaToken teremos acesso
     // ao ID da pessoa que criou a categoria
