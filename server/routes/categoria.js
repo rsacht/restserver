004371f20170fc130bf390bce81c5d6f795ff034
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-24 13:05:43 -0300

    Preparando o arquivo para exportação

--- server/routes/categoria.js
+++ server/routes/categoria.js
@@ -2,3 +2,5 @@ const express = require('express');
 let{verificaToken} = require('../middlewares/auth');
 let app = express();
 let Categoria = require('../models/categoria');
+
+module.exports = app;
