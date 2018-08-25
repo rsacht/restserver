2c4e3a9029b0fb0ea2800c86e8a60f4144b7d050
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-25 15:50:20 -0300

    Criando um novo produto - Estrutura Básica

--- server/routes/produtos.js
+++ server/routes/produtos.js
@@ -3,6 +3,11 @@ const{verificaToken} = require('../middlewares/auth');
 let app = express();
 let Produto = require('../models/produto');
 
+// ===========================
+// Cria um novo produto
+// ===========================
+app.post('/produtos', (req, res) =>{
 
+});
 
 module.exports = app;
