ae85678e4f981b27e1801263afbecdd665ea0dd1
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-25 15:44:04 -0300

    Exportando as configurações do objeto app

--- server/routes/produtos.js
+++ server/routes/produtos.js
@@ -2,3 +2,7 @@ const express = require ('express');
 const{verificaToken} = require('../middlewares/auth');
 let app = express();
 let Produto = require('../models/produto');
+
+
+
+module.exports = app;
