d3633716f05f3272410723ef088d8ec8340769b7
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-25 15:23:17 -0300

    Definindo as propriedades do modelo

--- server/models/produto.js
+++ server/models/produto.js
@@ -2,5 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var produtoSchema = new Schema({
-
+    nome:{type: String, required:[true, 'Digite o nome']},
+    precoUni:{type: Number, required:[true, 'Digite o preço unitário']},
+    descricao:{type: String, required:false},
+    categoria:{type:Schema.Types.ObjectId, ref:'Categoria', required:true},
+    usuario:{type:Schema.Types.ObjectId, ref:'Usuario'}
 });
