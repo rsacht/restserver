db940ca327bad32b0161564aa5d624426d9847a0
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-26 17:20:43 -0300

    Definindo a rota do serviço de upload

--- server/routes/uploads.js
+++ server/routes/uploads.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const app = express();
+
+// default options
+app.use(fileUpload());
+
+app.put('/upload', function(req, res) {
+
+});
