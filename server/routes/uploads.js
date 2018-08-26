b0775f37fa9455c033d0dcede76a902a2ddde4e6
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-26 17:22:49 -0300

    Validando o envio sem arquivo. Se o usuário não enviar arquivos o sistema retornará a mensagem de erro

--- server/routes/uploads.js
+++ server/routes/uploads.js
@@ -6,5 +6,6 @@ const app = express();
 app.use(fileUpload());
 
 app.put('/upload', function(req, res) {
-
+    if (!req.files)
+    return res.status(400).send('Não há arquivos para upload.');
 });
