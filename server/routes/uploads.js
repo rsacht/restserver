7d664ff14fdd3be9601315d43d745641324ef0ba
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-26 17:26:35 -0300

    Transformando o retorno de erro em um JSON

--- server/routes/uploads.js
+++ server/routes/uploads.js
@@ -6,6 +6,13 @@ const app = express();
 app.use(fileUpload());
 
 app.put('/upload', function(req, res) {
-    if (!req.files)
-    return res.status(400).send('Não há arquivos para upload.');
+    if (!req.files){
+        return res.status(400)
+            .json({
+                ok: false,
+                err: {
+                    message: 'Não há arquivos para upload.'
+            }
+        });
+    }
 });
