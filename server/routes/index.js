666546d1a4111fbdf19d954641413762e966d098
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-26 17:42:11 -0300

    Registrando a rota uploads

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,5 +6,6 @@ app.use(require('./usuario'));
 app.use(require('./login'));
 app.use(require('./categorias'));
 app.use(require('./produtos'));
+app.use(require('./uploads'));
 
 module.exports = app;
