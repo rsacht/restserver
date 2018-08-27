1dfe851a6806983e2c904f66a503b42fb1326e74
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-27 20:18:26 -0300

    Retornando uma imagem - Teste. sendfile retorna qualquer tipo de arquivo enviado

--- server/routes/imagens.js
+++ server/routes/imagens.js
@@ -3,10 +3,12 @@ const fs = require('fs');
 
 let app = express();
 
-app.get('/:destinatario/:img', (req, resp) =>{
+app.get('/imagem/:destinatario/:img', (req, res) =>{
     let destinatario = req.params.destinatario;
     let img = req.params.img;
     let pathImg = `./uploads/${destinatario}/${img}`;
+
+    res.sendfile('./server/assets/no-image.jpg');
 
 });
 
