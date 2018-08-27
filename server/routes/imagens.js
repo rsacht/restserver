fd64374eb3780fa1003de637c1d42ccfc0042be0
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-27 20:29:33 -0300

    Definindo o path absoluto da imagem no caso de upload

--- server/routes/imagens.js
+++ server/routes/imagens.js
@@ -7,7 +7,8 @@ let app = express();
 app.get('/imagem/:destinatario/:img', (req, res) =>{
     let destinatario = req.params.destinatario;
     let img = req.params.img;
-    let pathImg = `./uploads/${destinatario}/${img}`;
+
+    let pathImagem = path.resolve(__dirname, `../../uploads/${destinatario}/${img}`);
     let noImagePath = path.resolve(__dirname,'../assets/no-image.jpg')
 
     res.sendfile(noImagePath);
