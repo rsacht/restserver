6781b781d72dae1d009164df368d0af69f5486e5
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-27 16:13:46 -0300

    Definindo as variáveis passadas por parâmetro (destinatario e img)

--- server/routes/imagens.js
+++ server/routes/imagens.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 let app = express();
 
 app.get('/:destinatario/:img', (req, resp) =>{
-
+    let destinatario = req.params.destinatario;
+    let img = req.params.img;
+    
 });
 
 module.exports = app;
